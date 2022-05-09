8d08159bf263ba9b811e4555d5af3f76c9d9512c
Marcin <obcy82@gmail.com>
2022-05-09 19:51:26 +0200

    Initial commit

---
+++ src/main/java/com/loft/FurnitureShopApplication.java
@@ -0,0 +1,13 @@
+package com.loft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureShopApplication.class, args);
+	}
+
+}
