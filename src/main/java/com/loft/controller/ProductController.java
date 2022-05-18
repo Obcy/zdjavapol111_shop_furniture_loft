207d04bbb93527c357973bc506749c4b64769c3a
kitow <KitowskiA@gmail.com>
2022-05-18 19:59:44 +0200

    currency with values part1

--- src/main/java/com/loft/controller/ProductController.java
+++ src/main/java/com/loft/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.loft.controller;
 
 
+import com.loft.currency.model.CurrencyRate;
+import com.loft.currency.service.CurrencyRateService;
 import com.loft.model.User;
 import com.loft.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
@@ -9,7 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @Slf4j
@@ -18,9 +25,15 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping(path = "/productList")
-    public String productList(ModelMap modelMap) {
-        modelMap.addAttribute("Produkty", productService.getAll());
+    @Autowired
+    private CurrencyRateService currencyRateService;
+
+    @GetMapping(path = "/productList/{code}")
+    public String productList(@PathVariable String code, ModelMap modelMap) {
+        modelMap.addAttribute("products", productService.getAll(code));
+        List<CurrencyRate> currencyRates = currencyRateService.getCurrentRateByDate(LocalDate.now());
+        List<String> options = currencyRates.stream().map(currencyRate -> currencyRate.getCode()).collect(Collectors.toList());
+        modelMap.addAttribute("options", options);;
         return "products";
     }
 }
